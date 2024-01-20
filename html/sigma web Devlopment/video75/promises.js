3b1252cc0944e6499c3b107cdcdaa53b45fb0bfa
Ashish Vishwakarama <tanujgaming87@gmail.com>
2024-01-20 15:27:26 +0530

    Async/Await & Fetch API in JavaScript

--- html/sigma web Devlopment/video75/promises.js
+++ html/sigma web Devlopment/video75/promises.js
@@ -1,33 +1,31 @@
-console.log('This is Promises');
-
 let prom1 = new Promise((resolve, reject) => {
     let a = Math.random();
     if (a < 0.5) {
-        reject("No random number was not supporting you")
+        reject("I am reajered 1")
     }
     else {
         setTimeout(() => {
-            console.log("Yes I am done")
-            resolve("Harry")
-        }, 3000);
+            console.log("running 1")
+            resolve("resolved")
+        }, 2000);
     }
 })
 
 let prom2 = new Promise((resolve, reject) => {
     let a = Math.random();
     if (a < 0.5) {
-        reject("No random number was not supporting you 2")
+        reject("I am rejected 2")
     }
     else {
         setTimeout(() => {
-            console.log("Yes I am done 2")
-            resolve("Harry 2")
+            console.log("running 2")
+            resolve("resolved 2")
         }, 1000);
     }
 })
 
 
-let p3 = Promise.race([prom1, prom2])
+let p3 = Promise.allSettled([prom1, prom2])
 p3.then((a) => {
     console.log(a)
 }).catch(err => {
