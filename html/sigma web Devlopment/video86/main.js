9784098c6138864bbe37340fbeec948c2d66de3c
Ashish Vishwakarama <tanujgaming87@gmail.com>
2024-01-28 17:40:34 +0530

    type="modules\commanjs"

---
+++ html/sigma web Devlopment/video86/main.js
@@ -0,0 +1,28 @@
+// const http = require('node:http');
+// const hostname = '127.0.0.1';
+
+// // import http from 'http';
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end("Hello, world!");
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+// named export
+// import { a, b } from "./mymodule.js";
+// console.log(a, b);
+
+// default export
+// import ashu from "./mymodule.js";
+// console.log(ashu);
+
+// const b = require("./mymodule2.js");
+// console.log(b, __dirname, __filename);
---
+++ html/sigma web Devlopment/video86/mymodule.js
@@ -0,0 +1,12 @@
+// named export
+
+export const a = 89, b = 90;
+
+//default export
+let obj = {
+    myname: "Aashihs",
+    age: "14",
+    mothername: "xyz"
+}
+
+export default obj;
