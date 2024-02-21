37d3bfca1c16ea7f00a4f68e11ae4a9dfcee1908
Ashish Vishwakarama <tanujgaming87@gmail.com>
2024-02-21 13:15:53 +0530

    intro to react

---
+++ html/sigma web Devlopment/video106/react-first-app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar from './components/navbar'
+import Footer from './components/footer'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main>
+      
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
