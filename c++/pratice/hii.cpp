1e014d40f0a685bb03734bf5a6c8aa62e04196e6
Code-With-Aashish <tanujgaming87@gmail.com>
2024-04-11 09:09:34 +0530

    inital commint

--- c++/pratice/hii.cpp
+++ c++/pratice/hii.cpp
@@ -1,57 +1,56 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-int main(){
+int main()
+{
+    int i = 0;
     // loop in c++
     // are three type of loop
-   // 1.for loop
-   // 2.while loop
-   // 3.do while loop
+    // 1.for loop
+    // 2.while loop
+    // 3.do while loop
     // 1. for loop in c++
 
-//    for (int i = 0; i <= 100; i++)
-//    {
-//     cout<<i<<endl;
-    
-//     }
-// infinite loop of c++
+    //    for (int i = 0; i <= 100; i++)
+    //    {
+    //     cout<<i<<endl;
 
-//    for (int i = 0; 99 <= 100; i++)
-//    {
-//     cout<<i<<endl;
-    
-//     }
-   // 2.while loop
-//    
-//    while (i<=100)
-//    {
-//     cout<<i<<endl;
-//     i++;
-//    }
-//    infinite loop of while loop
-//  while (99<=100)
-//  {
-//     cout<<i<<endl;
-//     i++;
+    //     }
+    // infinite loop of c++
 
-//  }
-// 3. do while loop 
-// do 
-// {
-//      cout<<i<<endl;
-//      i++;
-// } 
-// while (i<=100);
+    //    for (int i = 0; 99 <= 100; i++)
+    //    {
+    //     cout<<i<<endl;
 
- 
-// do while loop in c++ run one time then cheak the condition
+    //     }
+    // 2.while loop
+    //
+    //    while (i<=100)
+    //    {
+    //     cout<<i<<endl;
+    //     i++;
+    //    }
+    // infinite loop of while loop
+    while (99 <= 100)
+    {
+        cout << i << endl;
+        i++;
+    }
+    // 3. do while loop
+    // do
+    // {
+    //      cout<<i<<endl;
+    //      i++;
+    // }
+    // while (i<=100);
 
-//    do
-// {
-//      cout<<i<<endl;
-//      i++;
-// } while (false);
+    // do while loop in c++ run one time then cheak the condition
+
+    //    do
+    // {
+    //      cout<<i<<endl;
+    //      i++;
+    // } while (false);
 
     return 0;
 }
